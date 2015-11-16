6d8e7ca97e33f02800d3e3344d5b9301bafc1923
Nathan Anderle <anderle.nathan8@gmail.com>
2015-11-15 19:15:05 -0500

    First commit with project file

---
+++ project2.py
@@ -0,0 +1,20 @@
+!#/usr/bin/enve python
+'''Operating System Loader Simulation
+
+This Python program is a graphical, interactive
+simulation of an operating system loader. It 
+allows the user to visualize the movement of
+processes, broken up into page tables, in 
+and out of the available physical memory which
+is broken up into frames. The simulated system
+in question has 4KB of physical memory and each
+page size is 512 bytes.
+'''
+
+__author__ = "Nathan Anderle"
+__version__ = "1.0"
+__email__ = "anderlna@mail.gvsu.edu"
+
+
+
+
