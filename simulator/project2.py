bdd0dca2ff9b715505062519637838b42e58ef23
Nathan Anderle <anderle.nathan8@gmail.com>
2015-11-17 14:35:16 -0500

    Implemented frame table. Further testing

--- simulator/project2.py
+++ simulator/project2.py
@@ -14,19 +14,33 @@ __author__ = "Nathan Anderle"
 __version__ = "1.0"
 __email__ = "anderlna@mail.gvsu.edu"
 
-class BasicFunctionality:
-    """See if I can figure out how the hell python works"""
-#    def __init__(self):
+class Setup:
+    """Reads in trace tape and sets up data structures"""
+   
+    def __init__(self, memdet, data):
+       """Initialize all the structures used by the simulation"""
+       self.trace = []
+       self.frames = dict.fromkeys(range(memdet))
+       self.data = data
 
-
-
-    def hello(self):
-        return "1Hello World"
-        
+    def read(self):
+        """Read line by line a trace tape file. Each line is a list in a list"""
+        try:
+            self.trace = [line.split() for line in open (self.data)]
+         
+        except IOError:
+            print "File does not exist"
+            return 1
+        else:
+            print self.trace
+            return self.trace        
 
 def main():
-    stuff = BasicFunctionality()
-    stuff.hello()
+    datafile = 'input2a.data'
+    memdet = 8 
+
+    stuff = Setup(memdet, datafile)
+    stuff.read()
 
 
 if __name__ == '__main__': main()
--- simulator/test.py
+++ simulator/test.py
@@ -2,20 +2,48 @@
 """Tests the simulator file for full functionality
 """
 import unittest 
-from project2 import BasicFunctionality
+from project2 import Setup
 
 __author__ = "Nathan Anderle"
 __version__ = "1.0"
 __email__ = "anderlna@mail.gvsu.edu"
 
-basic = BasicFunctionality()
 
-class BasicTestCase(unittest.TestCase):
-    """See if I can figure out how to python testi"""
+class TestSetup(unittest.TestCase):
+    """Execute a test suite on all methods in Setup"""
+  
+    def test_init_args(self):
+        """Are the correct # of args going to Setup?"""
+        data = 'input2a.data'
+        memdet = 8
+        self.assertTrue(Setup(memdet, data))
+        
+    def test_init_wargs(self):
+        """Error raised if incorrect number of args"""
+        data = 'input2a.data'
+        memdet = 8
+        self.assertRaises(Exception, Setup, memdet)
 
-    def test_is_hello(self):
-        """does hello return "Hello World"?"""
-        self.assertEqual(basic.hello(), "Hello World")
+    def test_read_size(self):
+        """Is the file the correct size?"""
+        data = 'input2a.data'
+        memdet = 8
+        setup = Setup(memdet, data)
+        self.assertEqual(len(setup.read()), 10)
+    
+    def test_read_exist(self):
+        """Does the data file exist?"""
+        data = 'input2a.data'
+        memdet = 8
+        setup = Setup(memdet, data)
+        self.assertTrue(setup.read())
+
+    def test_read_handled(self):
+        """Is the IOError handled correctly?"""
+        data = 'garbage'
+        memdet = 8
+        setup = Setup(memdet, data)
+        self.assertEqual(setup.read(), 1)
 
 
 if __name__ == '__main__': 
