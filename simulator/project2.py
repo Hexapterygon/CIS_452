07a113f3088e3571457bcf8f439f8097ec570a99
Nathan Anderle <anderle.nathan8@gmail.com>
2015-11-18 22:47:33 -0500

    Program works

--- simulator/project2.py
+++ simulator/project2.py
@@ -10,37 +10,102 @@ is broken up into frames. The simulated system
 in question has 4KB of physical memory and each
 page size is 512 bytes.
 """
+
+import math
+import collections
+from operator import itemgetter
+
 __author__ = "Nathan Anderle"
 __version__ = "1.0"
 __email__ = "anderlna@mail.gvsu.edu"
 
-class Setup:
+class Builder(object):
     """Reads in trace tape and sets up data structures"""
    
     def __init__(self, memdet, data):
-       """Initialize all the structures used by the simulation"""
+       """Initialize some date strutures used by the simulation"""
        self.trace = []
-       self.frames = dict.fromkeys(range(memdet))
-       self.data = data
+       
+       if(memdet <= 50):
+           self.memdet = memdet
+           self.frames = collections.OrderedDict.fromkeys(range(memdet))
+       else:
+           raise IOError
+           return 1
 
+       self.data = data
+       self.pages = []
+       
     def read(self):
         """Read line by line a trace tape file. Each line is a list in a list"""
         try:
             self.trace = [line.split() for line in open (self.data)]
-         
         except IOError:
             print "File does not exist"
             return 1
         else:
-            print self.trace
-            return self.trace        
+            return self.trace  
 
+    def frame_builder(self, procid, txtpages, datpages):
+       """TODO"""
+       i = 0
+       k = 0
+       for spaces in range(0, self.memdet, 1):
+                if(self.frames[spaces] is None and i < int(txtpages)):
+                    self.frames[spaces] = [procid, "TextPage " +str (i)] 
+                    i += 1
+                    self.page_builder(self.frames[spaces], spaces)
+                elif(self.frames[spaces] is None and k < int(datpages)):
+                    self.frames[spaces] = [procid, "DataPage " + str(k)] 
+                    k += 1
+                    self.page_builder(self.frames[spaces], spaces)
+
+    def page_builder(self, frameinfo, framenum):
+        """TODO"""
+        if not any(d.get(frameinfo[0], None) for d in self.pages):
+            t = {frameinfo[0] :  [str(frameinfo[1] +  " in Frame " + str(framenum))]}
+            self.pages.append(t.copy())
+        else:
+            for v in self.pages:
+              z = v.keys()
+              if (z[0] == frameinfo[0]):
+                 self.pages[self.pages.index(v)][frameinfo[0]].append( frameinfo[1] + " in Frame " + str(framenum)) 
+
+    def delete(self, victim):
+        """TODO"""
+        for frames in self.frames:
+            if self.frames[frames][0] is victim:
+               self.frames[frames] = 'None'
+        
+        for v in self.pages:
+            z = v.keys()
+            if(z[0] == victim):
+                del self.pages[self.pages.index(v)]
+class Simulate(object):
+    """Run the operating system loader simulation"""
+   
+    def __init__(self, trace, build):
+        """Initialize class variables"""
+        
+        self.trace = trace
+        self.build = build
+    
+    def simulate(self):
+        """TODO """
+        for entry in self.trace:
+            if(entry[1].isdigit()):
+                self.build.frame_builder(entry[0], math.ceil((float) (entry[1])/(512)), math.ceil((float) (entry[2])/(512)))
+            else:
+                self.build.delete(entry[0])
+        print self.build.frames
+        print self.build.pages
 def main():
     datafile = 'input2a.data'
     memdet = 8 
 
-    stuff = Setup(memdet, datafile)
-    stuff.read()
-
+    build = Builder(memdet, datafile)
+    simulate = Simulate(build.read(), build)
+    
+    simulate.simulate()
 
 if __name__ == '__main__': main()
--- simulator/test.py
+++ simulator/test.py
@@ -1,49 +1,77 @@
 #!/usr/bin/env python
 """Tests the simulator file for full functionality
+
+    W's tied to inidividual tests implies that the 
+    test is checking for something intentionally
+    incorrect
 """
 import unittest 
-from project2 import Setup
+from project2 import Builder
+from project2 import Simulate
 
 __author__ = "Nathan Anderle"
 __version__ = "1.0"
 __email__ = "anderlna@mail.gvsu.edu"
 
 
-class TestSetup(unittest.TestCase):
+class TestBuilder(unittest.TestCase):
     """Execute a test suite on all methods in Setup"""
   
     def test_init_args(self):
         """Are the correct # of args going to Setup?"""
         data = 'input2a.data'
         memdet = 8
-        self.assertTrue(Setup(memdet, data))
+        self.assertTrue(Builder(memdet, data))
         
     def test_init_wargs(self):
         """Error raised if incorrect number of args"""
         data = 'input2a.data'
         memdet = 8
-        self.assertRaises(Exception, Setup, memdet)
+        self.assertRaises(Exception, Builder, memdet)
+
+    def test_init_memdet(self):
+        """Is the physical memory determinate reasonable?"""
+        data = 'input2a.data'
+        memdet = 8
+        self.assertTrue(Builder(memdet, data))
+
+    def test_init_wmemdet(self):
+        """Is the physical memory determinate reasonable?"""
+        data = 'input2a.data'
+        memdet = 1000 
+        self.assertRaises(IOError, Builder, memdet, data)
 
     def test_read_size(self):
         """Is the file the correct size?"""
         data = 'input2a.data'
         memdet = 8
-        setup = Setup(memdet, data)
-        self.assertEqual(len(setup.read()), 10)
+        builder = Builder(memdet, data)
+        self.assertEqual(len(builder.read()), 10)
     
     def test_read_exist(self):
         """Does the data file exist?"""
         data = 'input2a.data'
         memdet = 8
-        setup = Setup(memdet, data)
-        self.assertTrue(setup.read())
+        builder = Builder(memdet, data)
+        self.assertTrue(builder.read())
 
     def test_read_handled(self):
         """Is the IOError handled correctly?"""
         data = 'garbage'
         memdet = 8
-        setup = Setup(memdet, data)
-        self.assertEqual(setup.read(), 1)
+        builder = Builder(memdet, data)
+        self.assertEqual(builder.read(), 1)
+
+class TestSimulate(unittest.TestCase):
+    """Test all methods in the Simulate class""" 
+
+    def test_init_args(self):
+        """Test the number of args to Simulate"""
+        trace = [1,2,3,4,5]  
+        data = 'input2a.data'
+        memdet = 8
+        builder = Builder(memdet, data)
+        self.assertTrue(Simulate(trace, builder))
 
 
 if __name__ == '__main__': 
